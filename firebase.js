2d7e07138748bc3de14112b474a264cfd7358006
shaikhusama155 <shaikhusama155@gmail.com>
2023-07-30 01:35:38 +0500

    facebook

---
+++ firebase.js
@@ -0,0 +1,87 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-analytics.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-firestore.js";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCMTPAhUuVd66aqZyqHLAXpeZu2-RoF3y0",
+  authDomain: "first-project-2233.firebaseapp.com",
+  projectId: "first-project-2233",
+  storageBucket: "first-project-2233.appspot.com",
+  messagingSenderId: "835896955200",
+  appId: "1:835896955200:web:03f9377b73f0edd6d9aaa5",
+  measurementId: "G-198NB1HHQQ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth();
+const db = getFirestore(app);
+
+
+let signbutton = document.getElementById("signup");
+signbutton.addEventListener("click", function () {
+  let email = document.getElementById("email");
+  let password = document.getElementById("password");
+  createUserWithEmailAndPassword(auth, email.value, password.value)
+    .then((userCredential) => {
+      // Signed in 
+      const user = userCredential.user;
+      console.log("user==>", user);
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log("erorr=>", errorMessage);
+      // ..
+    });
+})
+// const loginbutton = document.getElementById("login")
+// const signupbutton = document.getElementById("signup")
+// const mainE= document.getElementById("email")
+// const mainP= document.getElementById("password")
+
+// signupbutton.addEventListener("click", ()=>{
+// var email = mainE.value
+// var password = mainP.value
+//     createUserWithEmailAndPassword(auth, email, password)
+//   .then((userCredential) => {
+//     // Signed in
+//     const user = userCredential.user;
+// console.log(user);
+// alert("account create now you can login in this account")})
+//   .catch((error) => {
+//     const errorCode = error.code;
+//     const errorMessage = error.message;
+//     // ..
+//   });
+// })
+// loginbutton.addEventListener("click", ()=>{
+// var email = mainE.value
+// var password = mainP.value
+// signInWithEmailAndPassword(auth, email, password)
+//   .then((userCredential) => {
+//  location.replace("main.html")
+//     const user = userCredential.user;
+//     // ...
+//   })
+//   .catch((error) => {
+//     const errorCode = error.code;
+//     const errorMessage = error.message;
+//     // ..
+//   });
+// })
+
+// forgetpass.addEventListener(("click", () => {
+//   firebase.auth().sendPasswordResetEmail(email)
+//       .then(() => {
+//           alert("Reset link sent to your email id")
+//       })
+//       .catch((error) => {
+//           document.getElementById("error").innerHTML = error.message
+//       });
+// }))
